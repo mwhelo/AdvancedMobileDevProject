4a8879232150d76a13bec4325d6ad6868b5b7493
Matthew Whelan <matthew.whelan@student.ncirl.ie>
2017-03-24 13:08:42 +0000

    Login Added

--- app/src/main/java/com/example/x1243/amdproject/HomePage.java
+++ app/src/main/java/com/example/x1243/amdproject/HomePage.java
@@ -5,10 +5,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomePage extends AppCompatActivity implements View.OnClickListener {
     ImageView iv;
+
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textViewUserEmail;
+    private Button button_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,35 @@ public class HomePage extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this, LogIn.class));
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        button_logout = (Button) findViewById(R.id.button_logout);
+
+        button_logout.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == button_logout){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LogIn.class));
+        }
 
     }
 }
